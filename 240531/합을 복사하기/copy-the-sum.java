c2c5f49603395ab0990bbd3c0694f3185ad5867b
DaveLee-b <76063864+DaveLee-b@users.noreply.github.com>
2024-05-31 17:13:11 +0900

    Title: 합을 복사하기, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240531/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b= 2;
+        int c=3;
+        int t=0;
+        t=a+b+c;
+        a=t;
+        b=t;
+        c=t;
+        System.out.printf("%d %d %d",a,b,c);
+    }
+}
