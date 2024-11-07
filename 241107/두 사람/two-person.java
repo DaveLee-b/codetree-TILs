ecffe0e275d6669ec11d3f5c2771d185e66b8cbc
DaveLee-b <76063864+DaveLee-b@users.noreply.github.com>
2024-11-07 09:11:16 +0900

    Title: 두 사람, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241107/두 사람/two-person.java
+++ 241107/두 사람/two-person.java
@@ -1,22 +1,24 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc =  new Scanner(System.in);
-        int a_1 = sc.nextInt();
-        String s_1 = sc.next();
-        int a_2 = sc.nextInt();
-        String s_2 = sc.next();
-        if(a_1>19||a_2>19){
-            if(a_1>19&&s_1.equals("M")){
-                System.out.print("1");
-                
-            }else if(a_2>19&&s_2.equals("M")){
-                System.out.print("1");
-                
-            }else{
-                System.out.print("0");
-            }
+        Scanner sc = new Scanner(System.in);
+        
+        // 첫 번째 사람의 나이와 성별 입력
+        int age1 = sc.nextInt();
+        String gender1 = sc.next();
+        
+        // 두 번째 사람의 나이와 성별 입력
+        int age2 = sc.nextInt();
+        String gender2 = sc.next();
+        
+        // 조건에 따라 출력 결정
+        if ((age1 >= 19 && gender1.equals("M")) || (age2 >= 19 && gender2.equals("M"))) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
         }
+        
+        sc.close(); // Scanner 닫기
     }
 }
